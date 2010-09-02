57c51b075385aa437354974f8d809bf7593b5c39
Senthil <vss123@gmail.com>
2010-09-02 15:11:58 +0530

    Implemented multi-cell block

--- js/maze.js
+++ js/maze.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   $('#maze td').click(function(event){
     $(event.target).toggleClass('blocked cell_hover')
+  }),
+  $('#maze td').mouseover(function(event){
+    $(event.target).addClass('cell_hover')
+  }),
+  $('#maze td').mouseout(function(event){
+    $(event.target).removeClass('cell_hover')
+  }),
+  $('#maze td').mousedown(function(){
+    $('#maze td').hover(function(event){
+      $(event.target).addClass('blocked')
+    })
+  }),
+  $('#maze td').mouseup(function(){
+    $('#maze td').unbind('mouseenter mouseleave')
   })
 })
