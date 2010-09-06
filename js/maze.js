8aa05114751d78ccde213208e0f81a1a2e54947a
Senthil <vss123@gmail.com>
2010-09-06 23:34:35 +0530

    Removed hover effect with mouse over deselect

--- js/maze.js
+++ js/maze.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
   $('#maze td').click(function(event){
-    $(event.target).toggleClass('blocked cell_hover')
+    $(event.target).toggleClass('blocked')
   }),
-  $('#maze td').mouseover(function(event){
-    $(event.target).addClass('cell_hover')
-  }),
-  $('#maze td').mouseout(function(event){
-    $(event.target).removeClass('cell_hover')
-  }),
+
   $('#maze td').mousedown(function(){
-    $('#maze td').hover(function(event){
-      $(event.target).addClass('blocked')
+    $('#maze td').mouseleave(function(event){
+      $(event.target).toggleClass('blocked')
     })
   }),
+
   $('#maze td').mouseup(function(){
-    $('#maze td').unbind('mouseenter mouseleave')
+    $('#maze td').unbind('mouseleave')
   })
 })
